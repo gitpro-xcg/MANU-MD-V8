5bb6f350590b4471acbfa92a7186834a3141bf3d
gitpro-xcg <fa6562332@gmail.com>
2025-05-25 21:09:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -41,6 +41,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Put_Your_Session_ID', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&aEFkTC4S#WZjnZfIKdMD63gIG7AT4MdSHsOqEPCfcuUZKHqPdK1U', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000'
 };
